28617fd2238567958f15afabbf9f1975457ba2e1
yash chotaliya <yashchotaliya187@gmail.com>
2024-06-29 16:28:33 +0530

    login, register api called

--- OOHClient/app/src/main/java/com/example/oohclient/commonactivities/Register.kt
+++ OOHClient/app/src/main/java/com/example/oohclient/commonactivities/Register.kt
@@ -2,15 +2,22 @@ package com.example.oohclient.commonactivities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.oohclient.R
 import com.example.oohclient.databinding.ActivityRegisterBinding
 import com.example.oohclient.model.UserRegisterLoginModel
+import org.json.JSONException
+import java.lang.reflect.Method
+import org.json.JSONObject
 
 class Register : AppCompatActivity() {
 
@@ -50,6 +57,51 @@ class Register : AppCompatActivity() {
             confirmPassword = binding.confirmPassword.text.toString()
         )
 
-        Toast.makeText(this, "User registered", Toast.LENGTH_SHORT).show()
+        if(userRegisterLoginModel.password!=userRegisterLoginModel.confirmPassword){
+            Toast.makeText(this,"Both passwords are different. Try again", Toast.LENGTH_LONG).show()
+        }
+        else{
+            val queue = Volley.newRequestQueue(this@Register)
+
+            val request: StringRequest =
+                object : StringRequest(
+                    Method.POST, "https://odoo-combate.onrender.com/auth/signup",
+                    Response.Listener { response ->
+                        try {
+                            val jsonObject = JSONObject(response!!)
+
+                            val status = jsonObject.get("status").toString()
+                            val message = jsonObject.get("message").toString()
+                            if(status=="201"){
+                                startActivity(Intent(this@Register, Login::class.java))
+                                finish()
+                            }
+                            else{
+                                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                            }
+                        } catch (e: JSONException) {
+                            Toast.makeText(this, "Try Again", Toast.LENGTH_SHORT).show()
+                        }
+                    }, Response.ErrorListener { error ->
+                        Toast.makeText(this, error.message.toString(), Toast.LENGTH_SHORT).show()
+                    }) {
+                    override fun getParams(): Map<String, String> {
+
+                        val params: MutableMap<String, String> = HashMap()
+
+                        params["email"] = userRegisterLoginModel.email.toString()
+                        params["name"] = userRegisterLoginModel.name.toString()
+                        params["password"] = userRegisterLoginModel.password.toString()
+                        params["role"] = userRegisterLoginModel.userRole.toString()
+
+                        return params
+                    }
+                }
+            queue.add(request)
+        }
+
+
+
+
     }
 }
